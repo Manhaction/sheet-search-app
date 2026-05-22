e99a60bf7eb90a924a808c395c88826f1b3f57cb
Manhaction <pei.mine@gmail.com>
2026-05-22 23:58:07 +0700

    convert to public sheet csv

--- app.py
+++ app.py
@@ -1,45 +1,31 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Google Sheets setup
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive"
-]
-
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    "service_account.json",
-    scope
-)
-
-client = gspread.authorize(creds)
-
-# Mở Google Sheet
-sheet = client.open_by_key("1l8T_xs-mmCmNy_p4BArR0WzlkRt85drj-yHkqdG5mws").sheet1
-
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1l8T_xs-mmCmNy_p4BArR0WzlkRt85drj-yHkqdG5mws/export?format=csv"
 
 @app.route('/search')
 def search_data():
+
     keyword = request.args.get('q', '').lower()
 
-    data = sheet.get_all_records()
+    df = pd.read_csv(SHEET_URL)
 
     results = []
 
-    for row in data:
-        if any(keyword in str(value).lower() for value in row.values()):
-            results.append(row)
+    for _, row in df.iterrows():
+
+        row_dict = row.to_dict()
+
+        if any(keyword in str(value).lower() for value in row_dict.values()):
+            results.append(row_dict)
 
     return jsonify(results)
 
-
-import os
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
